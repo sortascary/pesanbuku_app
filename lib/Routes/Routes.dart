aca78558ac4227261ffd139a2e6fdb1a65f95c0a
sortascary <dkaky2008@gmail.com>
2025-03-09 21:52:20 +0700

    [DZ] testing out the OTP sign up

--- lib/Routes/Routes.dart
+++ lib/Routes/Routes.dart
@@ -1,3 +1,36 @@
-class MyRoutes {
+import 'package:get/get.dart';
+import 'package:pesanbuku_app/Bindings/test_register_Binding.dart';
+import 'package:pesanbuku_app/Pages/Login_page.dart';
+import 'package:pesanbuku_app/Pages/test_OTP_page.dart';
+import 'package:pesanbuku_app/Pages/test_register_page.dart';
 
+class MyRoutes {
+  static const String login = '/';
+  static const String register = '/register';
+  static const String dash = '/dash';
+  static const String notif = '/notif';
+
+  //this is just for testing
+  static const String testR = '/testR';
+  static const String testOTP = '/testOTP';
+}
+
+class AppPages{
+  static final List<GetPage<dynamic>> pages = <GetPage<dynamic>>[
+    GetPage(
+      name: MyRoutes.login,
+      page: () => LoginPage(),
+      //TODO: add binding
+    ),
+    GetPage(
+      name: MyRoutes.testR,
+      page: () => TestRegisterPage(),
+      binding: TestRegisterBinding(),
+    ),
+    GetPage(
+      name: MyRoutes.testOTP,
+      page: () => TestOtpPage(),
+      binding: TestRegisterBinding(),
+    ),
+  ];
 }
