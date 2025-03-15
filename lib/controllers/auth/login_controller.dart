778755c49c802e8e672bd7c0291802221ee1b215
sortascary <dkaky2008@gmail.com>
2025-03-15 11:41:06 +0700

    [DZ] resolving conflict

--- lib/controllers/auth/login_controller.dart
+++ lib/controllers/auth/login_controller.dart
@@ -2,28 +2,61 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pesanbuku_app/Routes/Routes.dart';
+import 'package:pesanbuku_app/api/auth/auth_service.dart';
 
 class LoginController extends GetxController {
-  var username = ''.obs;
-  var password = ''.obs;
+final AuthService auth = AuthService();
+
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+   //validator error
+  RxString phoneError = ''.obs;
+  RxString passError = ''.obs;
+
   var userType = ''.obs;
   var isLoading = false.obs;
 
   Future<void> login() async {
+    try {
     isLoading.value = true;
-    await Future.delayed(Duration(seconds: 2));
-
-    if (username.value == "admin" && password.value == "123") {
+      final fcmToken = await FirebaseMessaging.instance.getToken();
+      if (phoneController.text == "admin" && passwordController.text == "123") {
       userType.value = "admin";
-      Get.offNamed(MyRoutes.dashboardDistributor, arguments: {"userRole": "admin"}); 
-    } else if (username.value == "sekolah" && password.value == "123") {
+        Get.offNamed(MyRoutes.dashboardDistributor, arguments: {"userRole": userType.value}); 
+      } else if (phoneController.text == "sekolah" && passwordController.text == "123") {
       userType.value = "sekolah";
-      Get.offNamed(MyRoutes.dashboardSekolah, arguments: {"userRole": "sekolah"});  
+        Get.offNamed(MyRoutes.dashboardSekolah, arguments: {"userRole": userType.value});  
     } else {
       Get.snackbar("Error", "Username atau Password salah",
           snackPosition: SnackPosition.BOTTOM);
     }
 
-    isLoading.value = false;
+      // final response = await auth.login(
+      //     phoneController.text,
+      //     passwordController.text,
+      //     fcmToken!
+      // );
+      // print(phoneController.text);
+
+      // print(response.data['token']);
+      // userType.value = "admin";
+      // Get.offNamed(MyRoutes.dashboardDistributor, arguments: {"userRole": userType.value}); 
+    } catch (e){
+      isLoading(true);
+      Get.snackbar(
+        "Gagal",
+        "Silahkan cek email dan password anda! ",
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+        snackPosition: SnackPosition.TOP,
+        borderRadius: 30,
+        margin: EdgeInsets.all(10),
+      );
+      print(e);
+
+    } finally {
+      isLoading(false);
+    }
   }
 }
