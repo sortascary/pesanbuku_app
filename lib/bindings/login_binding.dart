5b2789ff856af69d31b563b1876fd269eaa51c9d
sortascary <66732198+sortascary@users.noreply.github.com>
2025-03-15 12:50:20 +0700

    Delete lib/bindings directory

--- lib/bindings/login_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:pesanbuku_app/controllers/auth/login_controller.dart';
-
-class LoginBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<LoginController>(() => LoginController());
-  }
-}
--- lib/bindings/register_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:pesanbuku_app/controllers/auth/register_controller.dart';
-
-class RegisterBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<RegisterController>(() => RegisterController());
-  }
-}
