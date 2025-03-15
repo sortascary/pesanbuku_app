7fbb0950407a65b71b79622d451761cb286c9770
sortascary <66732198+sortascary@users.noreply.github.com>
2025-03-15 12:52:10 +0700

    Delete lib/Controllers directory

--- lib/Controllers/bottom_nav_bar_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class BottomNavController extends GetxController {
-  var selectedIndex = 0.obs;
-
-  void changeTab(int index) {
-    selectedIndex.value = index;
-  }
-}
